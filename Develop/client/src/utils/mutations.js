162fa0a766fb5d2689b798ae49d0f06a81282a28
Eric Devine <80430372+BigPorg@users.noreply.github.com>
2021-08-15 12:40:29 -0400

    started adding Apollo stuff, looks like it works in App.js

--- Develop/client/src/utils/mutations.js
+++ Develop/client/src/utils/mutations.js
@@ -2,3 +2,26 @@
 // ADD_USER will execute addUser
 // SAVE_BOOK will execute saveBook
 // REMOVE_BOOK will execture removeBook
+import { gql } from '@apollo/client';
+
+export const CREATE_MATCHUP = gql`
+  mutation createMatchup($tech1: String!, $tech2: String!) {
+    createMatchup(tech1: $tech1, tech2: $tech2) {
+      _id
+      tech1
+      tech2
+    }
+  }
+`;
+
+export const CREATE_VOTE = gql`
+  mutation createVote($_id: String!, $techNum: Int!) {
+    createVote(_id: $_id, techNum: $techNum) {
+      _id
+      tech1
+      tech2
+      tech1_votes
+      tech2_votes
+    }
+  }
+`;
