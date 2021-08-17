8a5f0db8b49d2bd4bf9ce06d4e717cec91ce8dcc
Eric Devine <80430372+BigPorg@users.noreply.github.com>
2021-08-16 20:05:04 -0400

    added to components

--- Develop/client/src/components/SignupForm.js
+++ Develop/client/src/components/SignupForm.js
@@ -1,7 +1,8 @@
 // replace addUser functionality with ADD USER mutation functionality
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
+import { useMutation } from '@apollo/react-hooks';
+import { ADD_USER } from '../utils/mutations';
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
