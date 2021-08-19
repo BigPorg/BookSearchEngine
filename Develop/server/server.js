2a77f73631d2829757d80ccd5d90e3e33ebc5444
Eric Devine <80430372+BigPorg@users.noreply.github.com>
2021-08-19 15:08:21 -0400

    bcs server.js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -23,23 +23,20 @@ async function startServer() {
   server = new ApolloServer({ typeDefs, resolvers, context: authMiddleware, plugins: [ApolloServerPluginLandingPageGraphQLPlayground()], playground: true });
 
   await server.start();
-
   server.applyMiddleware({ app });
-  app.use(express.urlencoded({ extended: false }));
-  app.use(express.json());
 }
 
-startServer();
+startServer()
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 }
-
-// app.use(routes);
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
 
 // db.once('open', () => {
 //   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
