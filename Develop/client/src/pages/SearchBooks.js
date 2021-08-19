df0596b299c653d92c8c207335fd8b4d682f48d3
Eric Devine <80430372+BigPorg@users.noreply.github.com>
2021-08-19 13:04:19 -0400

    apollo clients

--- Develop/client/src/pages/SearchBooks.js
+++ Develop/client/src/pages/SearchBooks.js
@@ -59,7 +59,7 @@ const SearchBooks = () => {
 
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
-    const [saveBook, { error, data }] = useMutation(SAVE_BOOK);
+    // const [saveBook, { error, data }] = useMutation(SAVE_BOOK);
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
