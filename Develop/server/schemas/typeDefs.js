575b68394734da167ca2e4b9c294c2b211e01c9f
Eric Devine <80430372+BigPorg@users.noreply.github.com>
2021-08-29 13:19:34 -0400

    commas in typeDefs

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -40,8 +40,8 @@ const typeDefs = gql`
   }
   
   type Mutation {
-    login(email: String! password: String!): Auth
-    addUser(username: String! email: String! password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
     saveBook(bookData: bookInput!): User
     removeBook(bookId: ID!): User
   }
